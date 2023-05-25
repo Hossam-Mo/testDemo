23d7fc27c31594493be5b7784ea891c86b91cee5
Hossam-mo <hossam.m.pro@gmail.com>
2023-05-25 16:24:03 +0300

    first commit

---
+++ src/Demo.java
@@ -0,0 +1,19 @@
+public class Demo {
+
+    private int x,y;
+
+
+    public Demo(int x, int y){
+        this.x=x;
+        this.y=y;
+
+    }
+
+
+
+    public int biggerNum(){
+
+        return x >y ? x : y;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+
+
+
+
+    public static void main(String args[]){
+        Demo a = new Demo(2,3);
+        System.out.println(a.biggerNum());
+
+    }
+
+}
